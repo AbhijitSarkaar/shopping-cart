ae2c04d9c65158b698dd7eaea2aa15356f55c7fb
Abhijit Sarkar <abhijitsarkar@Abhijits-MacBook-Air.local>
2026-05-16 14:01:42 +0530

    spring cloud load balancer

--- cart/src/main/java/com/microservices/cart/CartApplication.java
+++ cart/src/main/java/com/microservices/cart/CartApplication.java
@@ -1,11 +1,17 @@
 package com.microservices.cart;
 
+import com.microservices.cart.config.LoadBalancerProductClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@LoadBalancerClient(
+		name = "product-service",
+		configuration = LoadBalancerProductClientConfig.class
+)
 public class CartApplication {
 
 	public static void main(String[] args) {
